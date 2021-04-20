d97180369c88794fbaa7331b103b48dc60b39e53
kaloyanatanasov1 <kaloyanatanasov7@gmail.com>
2021-04-20 22:27:18 +0300

    Last commit!

--- src/main/java/com/qacourseproject/pages/BasePage.java
+++ src/main/java/com/qacourseproject/pages/BasePage.java
@@ -2,13 +2,34 @@ package com.qacourseproject.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
 
     protected final WebDriver driver;
 
-    public BasePage (WebDriver driver){
+    public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+
+    protected <T> T executeOperationWithExplicitWait(long timeOutInSeconds, ExpectedCondition<T> expectedCondition, int implicitWait) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        turnOffImplicitWait(driver);
+        T result = wait.until(expectedCondition);
+        turnOnImplicitWait(driver, implicitWait);
+        return result;
+    }
+
+    private void turnOnImplicitWait(WebDriver driver, int implicitWait) {
+        driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
+    }
+
+    private void turnOffImplicitWait(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     }
 }
--- src/main/java/com/qacourseproject/pages/MainStatsPage.java
+++ src/main/java/com/qacourseproject/pages/MainStatsPage.java
@@ -4,16 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MainStatsPage extends BasePage {
 
-    @FindBy(xpath = "//span[contains(text()=’Statistics’)]")
+    @FindBy(xpath = "//span[contains(text(),'Statistics')]")
     private WebElement statisticsBtn;
 
     @FindBy(xpath = "//td[@data-test='DIVIDEND_AND_YIELD-value']")
     private WebElement dividendsVal;
 
     public String getDividendsValue(){
+        executeOperationWithExplicitWait(10, ExpectedConditions.visibilityOf(dividendsVal), 4);
         return dividendsVal.getText();
     }
 
